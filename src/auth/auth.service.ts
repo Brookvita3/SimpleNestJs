3efe999ac0e36c5de10bd7915f178fa100680a22
Brook Vita <bachchinhtran@gmail.com>
2025-04-27 20:25:44 +0700

    feat(app): add simple authorization

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -13,19 +13,26 @@ export class AuthService {
   login(body: LoginDto) {
     const adminEmail = this.configService.get<string>('ADMIN_EMAIL');
     const adminPassword = this.configService.get<string>('ADMIN_PASSWORD');
+    const clientEmail = this.configService.get<string>('CLIENT_EMAIL');
+    const clientPassword = this.configService.get<string>('CLIENT_PASSWORD');
     const jwtSecret = this.configService.get<string>('JWT_SECRET');
 
-    if (body.email !== adminEmail || body.password !== adminPassword) {
-      throw new UnauthorizedException('Invalid credentials');
+    if (body.email === adminEmail && body.password === adminPassword) {
+      const payload = { sub: 'admin', email: body.email, role: 'admin' };
+      const access_token = this.jwtService.sign(payload, {
+        secret: jwtSecret,
+      });
+      return { access_token };
     }
 
-    const payload = { sub: 'admin', email: body.email };
-    const token = this.jwtService.sign(payload, {
-      secret: jwtSecret,
-    });
+    if (body.email === clientEmail && body.password === clientPassword) {
+      const payload = { sub: 'client', email: body.email, role: 'client' };
+      const access_token = this.jwtService.sign(payload, {
+        secret: jwtSecret,
+      });
+      return { access_token };
+    }
 
-    return {
-      access_token: token,
-    };
+    throw new UnauthorizedException('Invalid credentials');
   }
 }
